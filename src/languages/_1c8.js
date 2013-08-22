7b0f121af8cacc29620157d34c40bf9f177b4f04
andrewks777 <andrewks777@yahoo.de>
2013-08-22 20:51:53 +0400

    Rework and simplify templates for 1C

--- src/languages/_1c8.js
+++ src/languages/_1c8.js
@@ -5,9 +5,9 @@ Author: andrewks <andrewks777@yahoo.de>
 
 function(hljs){
   var IDENT_RE_RU = '[a-zA-Zа-яёА-ЯЁ_][a-zA-Z0-9а-яёА-ЯЁ_]*';
-  var OneS_KEYWORDS = 'если if тогда then иначеесли elsif иначе else конецесли endif цикл do для for по to из in каждого each пока while конеццикла enddo процедура procedure конецпроцедуры endprocedure функция function конецфункции endfunction перем var экспорт export перейти goto и and или or не not знач val прервать break продолжить continue возврат return попытка try исключение except конецпопытки endtry вызватьисключение raise выполнить execute добавитьобработчик addhandler удалитьобработчик removehandler истина true ложь false null неопределено undefined новый new';
-  var OneS_BUILT_IN = '';
-  var DQUOTE =  {className: 'dquote',  begin: '""'};
+  var KEYWORDS = 'если if тогда then иначеесли elsif иначе else конецесли endif цикл do для for по to из in каждого each пока while конеццикла enddo процедура procedure конецпроцедуры endprocedure функция function конецфункции endfunction перем var экспорт export перейти goto и and или or не not знач val прервать break продолжить continue возврат return попытка try исключение except конецпопытки endtry вызватьисключение raise выполнить execute добавитьобработчик addhandler удалитьобработчик removehandler истина true ложь false null неопределено undefined новый new';
+  var BUILT_IN = '';
+  var DQUOTE =  {className: '',  begin: '""'};
   var STR_START = {
       className: 'string',
       begin: '"', end: '"|$',
@@ -50,6 +50,10 @@ function(hljs){
     begin: '[\\+\\-\\*\\/%\\?\\(\\)\\[\\];:\\.\\,=]+',
     relevance: 0
   };
+  var PREPROCESSOR = {
+    className: 'preprocessor',
+    begin: '(#|&)', end: '$'
+  };
   var GOTO_LABEL = {
     className: 'goto-label',
     begin: '~' + IDENT_RE_RU
@@ -58,46 +62,16 @@ function(hljs){
   return {
     case_insensitive: true,
     lexems: IDENT_RE_RU,
-    keywords: {keyword: OneS_KEYWORDS, built_in: OneS_BUILT_IN},
+    keywords: {keyword: KEYWORDS, built_in: BUILT_IN},
     contains: [
       hljs.C_LINE_COMMENT_MODE,
       STR_START, STR_CONT,
+      PREPROCESSOR,
       GOTO_LABEL,
-      {
-        className: 'function',
-        begin: '\\b(процедура|procedure|функция|function)', end: '$',
-        lexems: IDENT_RE_RU,
-        keywords: 'процедура procedure функция function',
-        contains: [
-          {className: 'title', begin: IDENT_RE_RU},
-          {
-            className: 'tail',
-            endsWithParent: true,
-            contains: [
-              {
-                className: 'params',
-                begin: '\\(', end: '\\)',
-                lexems: IDENT_RE_RU,
-                keywords: 'знач val истина true ложь false null неопределено undefined',
-                contains: [STR_START, STR_CONT, NUMBER_MODE_WRAP_OP, NUMBER_MODE_WRAP_SPACE, DATE, OPERATOR_SYMB]
-              },
-              {
-                className: 'export',
-                begin: '(экспорт|export)', endsWithParent: true,
-                lexems: IDENT_RE_RU,
-                keywords: 'экспорт export',
-                contains: [hljs.C_LINE_COMMENT_MODE]
-              }
-            ]
-          },
-          hljs.C_LINE_COMMENT_MODE
-        ]
-      },
-      {className: 'preprocessor', begin: '(#|&)', end: '$'},
+      DATE,
       NUMBER_MODE_ALONE,
       NUMBER_MODE_WRAP_OP,
       NUMBER_MODE_WRAP_SPACE,
-      DATE,
       OPERATOR_SYMB
     ]
   };
